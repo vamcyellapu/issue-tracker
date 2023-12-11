61259b440fa6269109f7aebcf17666e1b981341f
vamcyellapu <vamcyellapu@gmail.com>
2023-12-11 08:39:59 +0530

    Build the nav bar

---
+++ app/NavBar.tsx
@@ -0,0 +1,45 @@
+"use client";
+import classNames from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+
+const NavBar = () => {
+  const currentPath = usePathname();
+  const links = [
+    {
+      href: "/",
+      label: "Dashboard",
+    },
+    {
+      href: "/issues",
+      label: "Issues",
+    },
+  ];
+  return (
+    <nav className="flex space-x-6 mb-5 border-b h-14 items-center px-5">
+      <Link href={"/"}>
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className={classNames({
+                "text-zinc-700": currentPath === link.href,
+                "text-zinc-400": currentPath !== link.href,
+                "hover:text-zinc-900 transition-all": true,
+              })}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
